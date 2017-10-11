cdbbb062458dffe70e4eae98acc01b6116981438
David <david@Knight.fm>
2017-10-10 20:51:49 -0700

    go

---
+++ pipelines/go.sh
@@ -0,0 +1,1 @@
+python extract_edges_pipeline.py --cloud --setup_file=/workspace/pix2pix-tensorflow/pipelines/setup.py --gcs_bucket=pix2pixdata --runner=DataflowRunner --project=pix2pix-182420 --staging_location=gs://pix2pixdata/staging  --temp_location=gs://pix2pixdata/temp
