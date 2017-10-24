a6f9e9972217f41f2a70e03fcf87465624077543
David <david@cloudwow.com>
2017-10-24 02:14:30 -0700

    omg

--- train_local.sh
+++ train_local.sh
@@ -1,4 +1,4 @@
-python trainer/mypix2pix.py --topic=southpark --max_steps=500000 \
+python trainer/mypix2pix.py --topic=southpark --max_steps=700000 \
        --checkpoint=./output \
        --output_dir=./output \
        --training_image_dir /ldata/data/southpark/train_images 
